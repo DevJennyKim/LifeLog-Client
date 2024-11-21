9197a015f1a2d4c661cc306051c9320186e3609c
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-21 00:52:07 -0700

    basic route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Header from './components/Header/Header';
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes></Routes>;
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/LoginRegisterForm/LoginRegister_Form.tsx
@@ -0,0 +1,6 @@
+import 'LoginRegister_Form.scss';
+function LoginRegister_Form() {
+  return <div>LoginRegister_Form</div>;
+}
+
+export default LoginRegister_Form;
