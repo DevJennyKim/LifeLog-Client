1641b8f8fbdfc056ec6f1430f4b272d9be28f690
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-25 13:26:22 -0700

    login feature done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import LoginRegister from './pages/LoginRegister/LoginRegister';
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
+import MainPage from './pages/MainPage/MainPage';
+import { AuthContextProvider, useAuth } from './context/AuthContext';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Header /> */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginRegister action="login" />} />
-          <Route
-            path="/register"
-            element={<LoginRegister action="register" />}
-          />
-        </Routes>
+        <AuthContextProvider>
+          <ProtectedRouteHeader />
+          <Routes>
+            <Route path="/" element={<ProtectedRoute />} />
+            <Route path="/login" element={<LoginRegister />} />
+            <Route path="/register" element={<LoginRegister />} />
+          </Routes>
+        </AuthContextProvider>
       </BrowserRouter>
     </>
   );
+}
+
+function ProtectedRoute() {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <MainPage /> : <HomePage />;
+}
+function ProtectedRouteHeader() {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Header /> : '';
 }
 
 export default App;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,27 +1,43 @@
-const { default: axios } = require('axios');
-const { createContext } = require('react');
+import axios from 'axios';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext(null);
+const useAuth = () => useContext(AuthContext);
+
+const getCookie = (name) => {
+  const cookieArr = document.cookie.split('; ');
+  for (const cookie of cookieArr) {
+    const [key, value] = cookie.split('=');
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+};
 
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
     JSON.parse(localStorage.getItem('user')) || null
   );
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
-
   const login = async (inputs) => {
     const url = '/api/auth/login';
     const response = await axios.post(`${baseUrl}${url}`, inputs, {
       withCredentials: true,
     });
-    console.log('user = ', response.data.user);
-    setCurrentUser(response.data.user);
+
+    const user = response.data.user;
+    console.log(response.data);
+    setCurrentUser(user);
+    localStorage.setItem('user', JSON.stringify(user));
     document.cookie = `access_token=${response.data.token}; path=/; max-age=7200;`;
   };
   const logout = async () => {
     setCurrentUser(null);
     localStorage.removeItem('user');
     document.cookie = 'access_token=; path=/; max-age=0;';
+    setIsAuthenticated(false);
   };
   useEffect(() => {
     if (currentUser) {
@@ -31,11 +47,18 @@ const AuthContextProvider = ({ children }) => {
     }
   }, [currentUser]);
 
+  useEffect(() => {
+    const token = getCookie('access_token');
+    setIsAuthenticated(!!token);
+  }, [currentUser]);
+
   return (
-    <AuthContext.Provider value={{ currentUser, login, logout }}>
+    <AuthContext.Provider
+      value={{ currentUser, login, logout, isAuthenticated }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
 
-export { AuthContext, AuthContextProvider };
+export { AuthContext, AuthContextProvider, useAuth };
